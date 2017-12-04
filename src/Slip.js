ad6d6396ce346a7a64b111be32e50549521054f9
Nick <ngideo@gmail.com>
2017-12-03 18:26:28 -0600

    simple math works

--- src/Slip.js
+++ src/Slip.js
@@ -25,7 +25,7 @@ export function readFromTokens(tokens) {
   
   var token = tokens.shift();
   if (token === undefined) 
-  return ''; //This is to appease the type-checker.. better way?
+    return ''; 
   else if (token === '('){
     var L = [];
     while (tokens[0] !== ')') 
@@ -47,21 +47,88 @@ export function atom(token) {
     return token;
 }
 
-let Env = {
-  names: {
-    
-  },
-  procedures: {
-  '+': (a, b) => a + b,
-  '-': (a, b) => a - b,
-  '*': (a, b) => a * b,
-  '/': (a, b) => a / b,
-  '>': (a, b) => a > b,
-  '<': (a, b) => a < b,
-  '>=': (a, b) => a >= b,
-  '<=': (a, b) => a <= b,
-  '=': (a, b) => a === b,
+export function stdEnv() {
+  //Add Math constants
+
+  let Env = {
+    names: {
+
+    },
+    procedures: {
+      //Math 
+      '+': (a, b) => a + b,
+      '-': (a, b) => a - b,
+      '*': (a, b) => a * b,
+      '/': (a, b) => a / b,
+
+      //Logic
+      '>': (a, b) => a > b,
+      '<': (a, b) => a < b,
+      '>=': (a, b) => a >= b,
+      '<=': (a, b) => a <= b,
+      '=': (a, b) => a === b,
+    }
   }
+  return Env;
+}
+
+var global_env = stdEnv();
+
+/**
+ * 
+ * @param {number | string | array} exp 
+ * @param {object} env 
+ */
+export function slipEval(exp, env=global_env) {
+  //console.log(first)
+  //console.log(rest)
+
+  //First two tests handle if exp is a Symbol
+  //A symbol is either a number or a name:value binding
+  //TODO: implement strings as Symbols. 
+  //TODO: allow names to be functions to handle:
+  //  (def f (func-returning-function))
+  if(env.names[exp]) {
+    return env.names[exp]
+  }
+  else if(typeof exp === 'number') {
+    return exp
+  }
+
+  //SPECIAL FORMS
+  else if (exp[0] === 'if'){
+    var [_, test, conseq, alt] = exp;
+    var newExp = slipEval(test, env) ? conseq : alt;
+    return slipEval(newExp, env);  
+  }
+  else if (exp[0] === 'define'){
+    var [_, symbol, valExp] = exp
+    return env.names[symbol] = slipEval(valExp, env);
+  }
+
+  //Handles procedure calls
+  //So far names cannot hold functions and there
+  //isn't a way to define new functions. 
+  else if(typeof env.procedures[exp[0]] === 'function'){
+    var first, rest;
+    [first, ...rest] = exp;
+    
+    var proc = env.procedures[first];
+    
+    return proc.apply(null, rest.map((v) => {
+        //console.log(v)
+        return slipEval(v, env)
+      }));
+  } 
+  else if(!env.names[exp] || !env.procedures[exp]) {
+    throw new SyntaxError(exp + ' not defined')
+    return;
+  }
+  
+  
+
+    
+
 }
 
 function slip (programInput) {
--- src/Slip.test.js
+++ src/Slip.test.js
@@ -1,4 +1,4 @@
-import {tokenize, parse, atom} from './Slip';
+import {tokenize, parse, atom, stdEnv, slipEval} from './Slip';
 //import Tokenizer from './tokenizer';
 
 it("tokenizes", () => {
@@ -19,4 +19,41 @@ it('atomizes', () => {
 it('parses', () => {
   expect(parse("(begin (define r 10) (* pi (* r r)))"))
     .toEqual(['begin', ['define', 'r', 10], ['*', 'pi', ['*', 'r', 'r']]]);
+})
+
+it('calls', () => {
+  var env = stdEnv();
+  //console.log(env)
+  expect(env.procedures['*'](1,2)).toEqual(2);
+})
+
+it('evals', () => {
+  var env = stdEnv()
+
+
+  expect(slipEval(['*', 2, 2], env)).toEqual(4)
+
+  /**
+   * extra args are silently ignored. Fix??
+   */
+  expect(slipEval(['+', 1, 2, 3]), env)
+    .toEqual(3)
+
+
+  expect(slipEval(parse("(+ (- 1 2) 3)")))
+    .toEqual(2);
+
+  expect(slipEval(parse("(if (> 2 1) (- 2 1) (+ 2 1))")))
+    .toEqual(1);
+
+  expect(slipEval(parse("(define two (+ (- 1 2) 3))")))
+    .toEqual(2);
+  
+  /**
+   * calling an undefined name throws SyntaxError
+   
+  function wat() {slipEval(['wat', 1, 2])}
+  expect(wat)
+    .toThrow('wat not defined');
+  */
 })
